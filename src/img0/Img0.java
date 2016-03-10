a44a42ceb144bb8d6f0884c696fb389538ab4198
Acer <Acer@Acer-PC>
2016-03-10 12:13:55 +0100

    Imagenes, Pantalla de inicio, pokedex terminada, pokemon iniciales y Profesor Oak

---
+++ src/img0/Img0.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package img0;
+
+/**
+ *
+ * @author Acer
+ */
+public class Img0 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Img imagen1 = new Img();
+        imagen1.setVisible(true);*/
+        Barraprog barra1 = new Barraprog();
+        barra1.setVisible(true);
+        
+    }
+    
+}
